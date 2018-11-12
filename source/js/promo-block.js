99f0df07b6d3a095d8bdd6adc26ee4b589bb2f56
Brad Frost <brad@bradfrost.com>
2018-11-12 16:01:57 -0500

    Homepage design

--- source/js/promo-block.js
+++ source/js/promo-block.js
@@ -1,17 +1,19 @@
-const promoBlocks = document.querySelectorAll('.c-promo-block__body');
+(function(){
+    const promoBlocks = document.querySelectorAll('.c-promo-block__body');
 
-observer = new IntersectionObserver(entries => {
+    observer = new IntersectionObserver(entries => {
 
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-        entry.target.classList.remove('c-promo-block__body--initial');
-        console.log('boom');
-    }
-  });
-});
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.remove('c-promo-block__body--initial');
+        }
+    });
+    });
 
-promoBlocks.forEach(promoBlock => {
-    promoBlock.classList.add('c-promo-block__body--initial');
+    promoBlocks.forEach(promoBlock => {
+        promoBlock.classList.add('c-promo-block__body--initial');
 
-    observer.observe(promoBlock);
-});
+        observer.observe(promoBlock);
+    });
+
+})();
